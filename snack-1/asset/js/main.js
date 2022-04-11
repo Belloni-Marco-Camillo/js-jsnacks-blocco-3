93624c82c7949726f652c5bbf9b8cb3b6951fc6c
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-04-11 13:33:17 +0200

    first_snack_done

---
+++ snack-1/asset/js/main.js
@@ -0,0 +1,60 @@
+/* 
+-Crea un array composto da 10 automobili.
+
+-Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+*/
+
+const cars = [{
+    carBrand: 'audi',
+    model: 'tt',
+    fuels: 'benzina',
+}, {
+    carBrand: 'audi',
+    model: 'r8',
+    fuels: 'benzina',
+}, {
+    carBrand: 'audi',
+    model: 'q3',
+    fuels: 'diesel',
+}, {
+    carBrand: 'ford',
+    model: 'fiesta',
+    fuels: 'benzina',
+}, {
+    carBrand: 'ford',
+    model: 'mustung',
+    fuels: 'benzina',
+}, {
+    carBrand: 'bmw',
+    model: 'i3',
+    fuels: 'elettricità',
+}, {
+    carBrand: 'bmw',
+    model: 'i1',
+    fuels: 'elettricità',
+}, {
+    carBrand: 'volkswagen',
+    model: 'polo',
+    fuels: 'benzina',
+}, {
+    carBrand: 'volkswagen',
+    model: 'golf',
+    fuels: 'diesel',
+}, {
+    carBrand: 'volkswagen',
+    model: 'id.4',
+    fuels: 'elettricità',
+}];
+
+/* Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array. */
+
+const resultBenzina = cars.filter(car => car.fuels == `benzina`);
+const resultDiesel = cars.filter(car => car.fuels == `diesel`);
+const resultOther = cars.filter(car => car.fuels !== `diesel` && car.fuels !== `benzina`);
+
+
+
+console.log(resultBenzina);
+console.log(resultDiesel);
+console.log(resultOther);
